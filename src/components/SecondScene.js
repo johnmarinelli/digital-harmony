ca265203f40a04a31286598a2480a8e5e827dba9
John Marinelli <john.marinelli@internations.org>
2019-05-08 09:49:03 +0200

    GlitchRepeater

--- src/components/SecondScene.js
+++ src/components/SecondScene.js
@@ -5,6 +5,81 @@ import { animated as anim } from 'react-spring/three'
 import clock from '../util/Clock'
 import Background from './Background'
 
+const GlitchRepeat = ({ mesh }) => {
+  const groupRef = useRef()
+  const numRepeats = 25,
+    frameBreak = 10
+
+  let numFrames = 0
+
+  const [geometries] = useMemo(
+    () => {
+      const geometries = new Array(numRepeats)
+
+      for (let i = 0; i < numRepeats; ++i) {
+        geometries[i] = mesh.geometry.clone()
+      }
+
+      return [geometries]
+    },
+    [mesh]
+  )
+
+  useRender(() => {
+    const now = clock.getElapsedTime()
+
+    const x = Math.sin(now)
+    const y = Math.cos(now)
+    const z = Math.sin(now)
+    const children = groupRef.current.children
+    const main = children[0]
+
+    // like numFrames % 8 === 0, but cheaper
+    if ((numFrames & 7) === 0) {
+      for (let i = children.length - 1; i > 0; --i) {
+        let current = children[i]
+        let next = children[i - 1]
+
+        current.position.x = next.position.x
+        current.position.y = next.position.y
+        current.position.z = next.position.z
+
+        const scale = (children.length - i) / children.length
+        current.scale.x = scale //next.scale.x
+        current.scale.y = scale //next.scale.y
+        current.scale.z = scale //next.scale.z
+
+        current.rotation.x = next.rotation.x
+        current.rotation.y = next.rotation.y
+        current.rotation.z = next.rotation.z
+      }
+    }
+
+    main.position.x = x
+    main.position.y = y
+    main.position.z = z
+
+    main.rotation.x += 0.01
+    main.rotation.z += 0.005
+    numFrames++
+  })
+
+  return (
+    <group position={new THREE.Vector3(-2.0, 2.0, 0.0)} ref={groupRef}>
+      <anim.mesh name="mainMesh" geometry={mesh.geometry}>
+        <anim.lineBasicMaterial name="material" color="pink" transparent={true} opacity={0.5} />
+      </anim.mesh>
+      {geometries.map((geometry, i) => {
+        return (
+          <anim.mesh key={i} name="mesh" geometry={geometry}>
+            <anim.lineBasicMaterial name="material" color="purple" wireframe={true} />
+          </anim.mesh>
+        )
+      })}
+    </group>
+  )
+}
+
 const DifferentialMotion = props => {
   let group = useRef()
 
@@ -89,6 +164,9 @@ class SecondScene extends React.Component {
   render() {
     const { top, size } = this.props
     const scrollMax = size.height * 4.5
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    geometry.scale(0.5, 0.5, 0.5)
+    const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xff0000 }))
 
     return (
       <scene ref={this.sceneRef} background={new THREE.Color(0xff00ff)}>
@@ -99,7 +177,7 @@ class SecondScene extends React.Component {
           )}
         />
         <anim.group>
-          <DifferentialMotion {...this.differentialMotionProps} />
+          <GlitchRepeat mesh={mesh} />
         </anim.group>
       </scene>
     )
