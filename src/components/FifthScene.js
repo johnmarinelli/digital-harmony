bd000c730990a81d1247333db4f0cae485aa03a2
John Marinelli <john.marinelli@internations.org>
2019-05-21 23:48:08 +0200

    [wip]

--- src/components/FifthScene.js
+++ src/components/FifthScene.js
@@ -6,6 +6,86 @@ import { animated as anim } from 'react-spring/three'
 import clock from '../util/Clock'
 import { Uniforms, FragmentShader } from '../shaders/FifthScene'
 
+const Particles = () => {
+  const group = useRef()
+
+  const numParticles = 5
+  const particleGeometries = ['ico', 'dod', 'cyl']
+
+  const [particles] = useMemo(() => {
+    const particles = []
+    const getGeometry = (str, props) => {
+      if (str === 'ico') return <icosahedronBufferGeometry name="geometry" {...props} />
+      if (str === 'dod') return <dodecahedronBufferGeometry name="geometry" {...props} />
+      if (str === 'cyl') return <cylinderBufferGeometry name="geometry" {...props} />
+    }
+    let particleGeometriesIndex = 0
+    let particleGeometryFn = null
+    let x = 0.0,
+      y = 0.0,
+      z = 1.0
+
+    let radius = 0.3
+    let position = null
+    let geometry = null
+
+    let xScale = 0.0,
+      yScale = 0.0,
+      zScale = 0.0
+
+    for (let i = 0; i < numParticles; ++i) {
+      particleGeometriesIndex = Math.floor(Math.random() * 100.0) % particleGeometries.length
+      const geometryString = particleGeometries[particleGeometriesIndex]
+
+      x = (Math.random() - 0.5) * 7.0
+      y = (Math.random() - 0.5) * 7.0
+      radius = 0.1
+
+      xScale = 0.2 + (Math.random() - 0.5) * 0.01
+      yScale = 0.2 + (Math.random() - 0.5) * 0.01
+      zScale = 0.2 + (Math.random() - 0.5) * 0.01
+
+      //particle = new particleGeometryFn()
+      position = new THREE.Vector3(x, y, z)
+
+      const props = {
+        radius,
+      }
+
+      geometry = getGeometry(geometryString, props)
+
+      particles.push(
+        <mesh
+          key={i}
+          position={position}
+          scale={[xScale, yScale, zScale]}
+          material={new THREE.MeshLambertMaterial({ color: 'white' })}
+        >
+          {geometry}
+        </mesh>
+      )
+    }
+
+    return [particles]
+  })
+
+  useRender(() => {
+    const { current: { children } } = group
+
+    const numChildren = children.length
+    for (let i = 0; i < numChildren; ++i) {
+      const child = children[i]
+      child.position.x -= 0.01
+
+      if (child.position.x < -3.0) {
+        child.position.x = 3.0
+      }
+    }
+  })
+
+  return <group ref={group}>{particles}</group>
+}
+
 const DifferentialMotion = props => {
   let group = useRef()
 
@@ -57,8 +137,10 @@ const DifferentialMotion = props => {
 
   return (
     <group ref={group}>
+      <ambientLight color={0xfff} intensity={0.8} />
       <spotLight color={0xffffff} intensity={0.7} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
       {staticObjects}
+      <Particles />
     </group>
   )
 }
