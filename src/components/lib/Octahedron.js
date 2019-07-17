10dc4ef4fc5b0866a0bf1d02a91327fd4ea379eb
John Marinelli <john.marinelli@internations.org>
2019-07-17 15:49:09 -0700

    dat.gui <-> react-spring

--- src/components/lib/Octahedron.js
+++ src/components/lib/Octahedron.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import * as THREE from 'three/src/Three'
 import { useRender } from 'react-three-fiber'
 import { useSpring, animated } from 'react-spring/three'
 import midi from '../../util/WebMidi'
 import GuiOptions from '../Gui'
 import { OctahedronStates, Triggers } from '../states/scenes/Fifth'
+import * as AnimationHelper from '../../util/AnimationHelper'
+import clock from '../../util/Clock'
 
 const Octahedron = ({ position = [0, 0, 0], states = OctahedronStates }) => {
   const lineVertices = [[-1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]
   const [frame, setFrame] = useState(-1)
+  const scaleAnimationTime = 0.5 // one-way, in seconds
+  const mesh = useRef()
 
   let nextStateIndex = frame
   if (frame < states.length - 1) {
@@ -29,6 +33,7 @@ const Octahedron = ({ position = [0, 0, 0], states = OctahedronStates }) => {
         if (lastNotesMatch) {
           if (frame < states.length - 1) {
             setFrame(frame + 1)
+            GuiOptions.options.subjectState = frame + 1
           }
         }
       }
@@ -37,36 +42,34 @@ const Octahedron = ({ position = [0, 0, 0], states = OctahedronStates }) => {
   )
 
   useRender(() => {
-    if (GuiOptions.options.subjectStateOverride) {
-      setFrame(GuiOptions.options.subjectState)
+    let now = clock.getElapsedTime()
+    const { options: { subjectState, subjectStateOverride } } = GuiOptions
+
+    if (subjectStateOverride) {
+      setFrame(subjectState)
+      GuiOptions.syncFifthSceneOptions(octahedronProps)
+    } else {
+      const {
+        options: { octahedronOpacity, octahedronColor, octahedronScale: scale, octahedronRotation: rotation },
+      } = GuiOptions
+      const { current: { material, geometry } } = mesh
+
+      material.color.set(octahedronColor)
+      material.opacity = octahedronOpacity
+
+      mesh.current.scale.set(scale.x, scale.y, scale.z)
+      mesh.current.rotation.set(
+        THREE.Math.degToRad(rotation.x),
+        THREE.Math.degToRad(rotation.y),
+        THREE.Math.degToRad(rotation.z)
+      )
     }
   })
 
-  const {
-    options: {
-      subjectStateOverride,
-      octahedronOpacity,
-      octahedronColor,
-      octahedronScale: scale,
-      octahedronRotation: rotation,
-    },
-  } = GuiOptions
-
-  const newProps = subjectStateOverride
-    ? Object.assign({}, octahedronProps, {
-        'material-opacity': octahedronOpacity,
-        'material-color': octahedronColor,
-        scale,
-        rotation,
-      })
-    : octahedronProps
-
-  console.log(newProps)
-
   return (
-    <animated.mesh {...newProps}>
+    <animated.mesh {...octahedronProps} ref={mesh}>
       <octahedronGeometry attach="geometry" />
-      <meshStandardMaterial attach="material" color="grey" transparent />
+      <meshStandardMaterial attach="material" transparent />
     </animated.mesh>
   )
 }
